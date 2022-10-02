97884fad2c9c01d1e5042be8c17344460a499547
Rokas <rookas.rudzenskas@gmail.com>
2022-10-02 18:31:11 +0300

    ROkas

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,8 +7,8 @@ import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 
 export const App = () => {
-    const endpoint = "https://muddy-aged-panorama.solana-devnet.discover.quiknode.pro/0fe7822c98ade63f96ae1be8e82d17b26d57cacc/";
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const endpoint = "https://muddy-aged-panorama.solana-devnet.discover.quiknode.pro/0fe7822c98ade63f96ae1be8e82d17b26d57cacc/";
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
   return (
       <ConnectionProvider endpoint={endpoint}>
