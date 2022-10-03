1ec98d6d690cdf7fc805906e4d789fe72e97d1a8
Rokas <rookas.rudzenskas@gmail.com>
2022-10-03 11:08:58 +0300

    Rokas

--- app/src/context/Blog.jsx
+++ app/src/context/Blog.jsx
@@ -1,9 +1,21 @@
-import { createContext, useContext } from "react";
+import {createContext, useContext, useState} from "react";
+import {useAnchorWallet, useConnection, useWallet} from "@solana/wallet-adapter-react";
 
 const BlogContext = createContext();
 
 export const useBlog = () => {
   const context = useContext(BlogContext);
+  const [user, setUser] = useState();
+  const [initialized, setInitialized] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [transactionPending, setTransactionPending] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [lastPostId, setLastPostId] = useState();
+
+  const anchorWallet = useAnchorWallet();
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
   if (!context) {
     throw new Error("Parent must be wrapped inside PostsProvider");
   }
@@ -16,9 +28,15 @@ export const BlogProvider = ({ children }) => {
 
   return (
     <BlogContext.Provider
-      value={{
-
-      }}
+        value={{
+          user,
+          posts,
+          initialized,
+          initUser,
+          createPost,
+          showModal,
+          setShowModal,
+        }}
     >
       {children}
     </BlogContext.Provider>
